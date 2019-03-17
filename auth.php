713cda1f62de8e08f4ab46cd4ad6f19e891d79f5
sina panahi <s.panahi.m@gmail.com>
2019-03-17 10:41:08 +0300

    the information in this file will be shared between login.php and setup.php

---
+++ auth.php
@@ -0,0 +1,11 @@
+<?php 
+	/* 
+	Contains the login information to connect to SQL 
+	*/
+	//login information
+	$servername = 	"localhost";
+	$username = 	"sina";
+	$password = 	"Moradkandi1";
+	$db = 			"esltoolkit_2";
+
+?>
